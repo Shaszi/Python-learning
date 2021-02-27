8a210f6a8bb4a25777f5e994c5eb5ab9647f31ce
Shaszi <kamykpietras@gmail.com>
2021-02-27 11:51:30 +0100

    python learning

---
+++ index.py
@@ -0,0 +1,14 @@
+import os
+os.system("clear")
+
+first_name = "John"
+age = 18
+fav_pizza = {
+    "John": "pepperoni",
+    "Bob":"mushrom",
+    "Mary": "cheese"
+}
+name = True
+
+print (fav_pizza["Bob"])
+
